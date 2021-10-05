599f84d3aa9e79538020216ad7b9b16c258ee22a
Valerii Logvin <v.logvin@telesens.ua>
2021-10-05 09:09:32 +0300

    v-model:opt

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <Select
+      :options='options'
+      v-model:opt='selectedOptions'
+    />
+    {{selectedOptions}}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Select from './components/Select.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Select
+  },
+  data() {
+    return {
+      selectedOptions: {},
+      options: [
+        {
+          name: 'Option 1',
+          value: 1
+        },
+        {
+          name: 'Option 2',
+          value: 2
+        },
+        {
+          name: 'Option 3',
+          value: 3
+        },
+        {
+          name: 'Option 4',
+          value: 4
+        },
+        {
+          name: 'Option 5',
+          value: 5
+        },
+      ]
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Select.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="select">
+    <p class="select-selected">{{modelValue}}</p>
+    <ul class="select-options">
+      <li 
+        class="select-option"
+        v-for="{name, value} in options"
+        :key='value'
+        @click="selectOption({name, value})"
+      >{{name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array, 
+      default() {
+        return []
+      } 
+    },
+    modelValue : null
+  },
+  methods: {
+    selectOption (value) {
+      this.$emit("update:opt", value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
